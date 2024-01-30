59576a298d3da3d76b9eca3b71de3bf71096dd0d
Walen Soares <walen.almeida@gmail.com>
2024-01-30 20:19:46 -0300

    Aula 1

---
+++ Net6udemy/Net6udemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using Net6udemy.Model;
+
+namespace Net6udemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
